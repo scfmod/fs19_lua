958c5cc18236f95dea2724431f50a576a427e61a
Espen Killi <espen@inlecture.no>
2018-11-29 22:27:28 +0100

    Added g_fruitTypeManager global variable

---
+++ g_fruitTypeManager.lua
@@ -0,0 +1,20 @@
+-- lookup: misc/FruitTypeManager.lua
+g_fruitTypeManager = {}
+
+g_fruitTypeManager.fillTypeIndexToFruitTypeIndex = {}
+g_fruitTypeManager.fruitTypeIndexToWindrowFillTypeIndex = {}
+g_fruitTypeManager.numCategories = 9
+g_fruitTypeManager.indexToFruitType = {}
+g_fruitTypeManager.fruitTypes = {}
+g_fruitTypeManager.nameToIndex = {}
+g_fruitTypeManager.categoryToFruitTypes = {}
+g_fruitTypeManager.weedFruitType = {}
+g_fruitTypeManager.fruitTypeIndexToFillType = {}
+g_fruitTypeManager.converterNameToIndex = {}
+g_fruitTypeManager.indexToCategory = {}
+g_fruitTypeManager.categories = {}
+g_fruitTypeManager.windrowFillTypes = {}
+g_fruitTypeManager.nameToConverter = {}
+g_fruitTypeManager.loadedMapData = true
+g_fruitTypeManager.fruitTypeConverters = {}
+g_fruitTypeManager.nameToFruitType = {}
